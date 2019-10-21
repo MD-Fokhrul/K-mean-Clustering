d5077e125c97f42b2d21a081cd9896e59ce7440a
MD-Fokhrul <55605296+MD-Fokhrul@users.noreply.github.com>
2019-10-21 19:04:53 +0600

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -10,12 +10,13 @@ dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3, 4]].values
 
 # Feature Scaling
+
+# no need to do >>> becuse sklearn's KMeans function do it in background
+
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+X_test = sc_X.transform(X_test) """
 
 # visualise data without clustering
  
@@ -23,6 +24,7 @@ plt.scatter(X[:, 0], X[:, 1], label = 'Cluster')
 plt.xlabel('Annual Income (k$)')
 plt.ylabel('Spending Score (1-100)')
 plt.show()
+
 # Using the elbow method to find the optimal number of clusters
 from sklearn.cluster import KMeans
 wcss = []
@@ -39,6 +41,8 @@ plt.ylabel('WCSS')
 plt.show()
 
 # Fitting K-Means to the dataset
+# determine '5' is optimal cluster
+
 kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 42)
 y_kmeans = kmeans.fit_predict(X)
 
@@ -54,5 +58,4 @@ plt.xlabel('Annual Income (k$)')
 plt.ylabel('Spending Score (1-100)')
 plt.legend()
 plt.show()
-
 
